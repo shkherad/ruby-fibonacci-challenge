4ad3b19f0e3af84226eb4d2fcef82d0ca41ef791
shkherad <shkherad@bu.edu>
2016-06-14 09:44:25 -0400

    need to add previous fib variable to get first1
    need to get first one

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,5 +1,24 @@
 class FibonacciRange
+  include Enumerable
+
+  attr_reader :stop, :current_fib, :next_fib, :previous_fib
+
+  def each
+      next_fib = 1
+      while next_fib <= @stop
+        yield next_fib
+        next_fib += next_fib
+      end
+  end
+
+  def initialize(stop)
+    @stop = stop
+  end
+
 end
+
+new_range = FibonacciRange.new(10).to_a
+p new_range
 
 class FibonacciNumber
 end
